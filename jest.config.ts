565414744d809f41727ff55cf348fce6f40afa52
JohnAchicaizaG <159038108+JohnAchicaizaG@users.noreply.github.com>
2025-04-03 18:43:25 -0500

    se agrega eslint, prettier

--- jest.config.ts
+++ jest.config.ts
@@ -1,15 +1,14 @@
-import type { Config } from "jest";
+import type { Config } from 'jest';
 
 export default {
-  moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/src/$1",
-  },
-  coverageDirectory: "./coverage",
-  collectCoverageFrom: [
-    "src/app/**/*.ts",
-    "!<rootDir>/node_modules/",
-    "!<rootDir>/test/",
-  ],
-  preset: 'jest-preset-angular',
-
+	moduleNameMapper: {
+		'^@/(.*)$': '<rootDir>/src/$1',
+	},
+	coverageDirectory: './coverage',
+	collectCoverageFrom: [
+		'src/app/**/*.ts',
+		'!<rootDir>/node_modules/',
+		'!<rootDir>/test/',
+	],
+	preset: 'jest-preset-angular',
 } satisfies Config;
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,15 @@
-describe('demo test', () => {
-  it('should be true', () => {
-    expect(true).toBe(true);
-  });
+import { Spectator, createComponentFactory } from '@ngneat/spectator/jest';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+	let spectator: Spectator<AppComponent>;
+	const createComponent = createComponentFactory(AppComponent);
+
+	beforeEach(() => {
+		spectator = createComponent();
+	});
+
+	it('debería crear el componente', () => {
+		expect(spectator.component.title).toEqual('hola');
+	});
 });
